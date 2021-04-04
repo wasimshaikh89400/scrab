ece096415ee0838be85bccbb41b642a0a5b2b088
wasimshaikh89400 <72592698+wasimshaikh89400@users.noreply.github.com>
2021-04-05 03:14:56 +0530

    Add files via upload

---
+++ scrab.js
@@ -0,0 +1,10 @@
+
+//for restaurunt name =>
+
+var name = document.getElementsByClassName("sc-7kepeu-0")
+console.log(name.innerText)
+
+//for locatio
+
+var location = document.getElementsByClassName("eSsbyy")
+console.log(location.innerText)
